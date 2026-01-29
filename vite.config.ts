5b061bb9ca0de0f24a580bd9b8ecbb37ef5657df
KaeruShi <farel.ardiansyah404@gmail.com>
2026-01-29 10:20:35 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/",
+  base: process.env.VITE_BASE_PATH || "/KaeruShi.github.io",
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
