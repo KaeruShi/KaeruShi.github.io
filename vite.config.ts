09e76e7c59d976077ed6eb5782d82e709cdc5ab2
KaeruShi <farel.ardiansyah404@gmail.com>
2026-01-29 10:31:30 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,22 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   base: process.env.VITE_BASE_PATH || "/KaeruShi.github.io",
   resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "./process": "process/browser",
+      process: "process/browser",
+    },
   },
   plugins: [react(), svgr(), tailwindcss()],
+  define: {
+    "process.env": {},
+    global: "globalThis",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
 });
