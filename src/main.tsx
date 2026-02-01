792b9ac7b228301e562baa9ac0abb64ab9f6911c
KaeruShi <farel.ardiansyah404@gmail.com>
2026-02-01 08:38:53 +0700

    setup shadcn and add cool background

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { SpotlightBackground } from "./components/ui/spotlight.tsx";
+import { BackgroundBeams } from "./components/ui/background-beams.tsx";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <div className="flex flex-col">
+    <BackgroundBeams className="fixed -z-10 lg:opacity-60" />
+    <SpotlightBackground blur={100} colors={["rgba(126,87,194,0.3)"]} className="-z-10"/>
     <App />
-  </StrictMode>,
-)
+  </div>
+);
